5e40a3bdd5ab3c1d6ab845b609e8184c6644e42e
Joao Correa <joaocln88@gmail.com>
2025-02-11 22:46:01 -0300

    j

--- src/components/ui/customInput.jsx
+++ src/components/ui/customInput.jsx
@@ -13,7 +13,7 @@ const CustomInput = React.forwardRef(
     return (
       <div
         className={cn(
-          "m-0 flex h-9 items-center justify-center overflow-hidden rounded-md border border-input bg-transparent p-0 py-0 text-base shadow-sm transition-colors focus-within:outline-none focus-within:ring-1 focus-within:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "m-0 flex h-9 items-center justify-center overflow-hidden rounded-md border border-input bg-transparent p-0 py-0 text-base shadow-xs transition-colors focus-within:outline-hidden focus-within:ring-1 focus-within:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
       >
@@ -31,7 +31,7 @@ const CustomInput = React.forwardRef(
         <input
           type={type}
           className={cn(
-            "flex h-full w-full items-center justify-center bg-transparent py-1 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-full w-full items-center justify-center bg-transparent py-1 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
             className,
           )}
           ref={ref}
